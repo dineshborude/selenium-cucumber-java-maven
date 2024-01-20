60f4c28b2b6cf227297411dd309d8537668c152a
Dinesh Borude <borudedinesh2@gmail.com>
2024-01-21 03:27:37 +0530

    error resolved

--- src/test/java/stepdefs/LoginPage.java
+++ src/test/java/stepdefs/LoginPage.java
@@ -11,33 +11,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LoginPage {
 
     public static WebDriver driver;
+
     @Given(":User navigates to login page.")
     public void user_navigates_to_login_page() {
-
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-
-
         System.out.println("Given Block");
     }
 
-    @When(": User enters (.*) and (.*)")
-    public void user_successfully_enters_the_login_details(String username, String password) {
-        System.out.println("When Block");
-        driver.navigate().to("https://anupdamoda.github.io/AceOnlineShoePortal/SignIn.html");
+    @When(": User enters (.*) and (.*)$")
+    public void user_enters_user_and_pass(String username, String password) {
+        driver.get("https://anupdamoda.github.io/AceOnlineShoePortal/SignIn.html");
         driver.findElement(By.xpath("//input[@id='usr']")).sendKeys(username);
-        driver.findElement(By.xpath("//input[@id='pwd']")).sendKeys(password);
-        driver.findElement(By.xpath("//input[@type='submit']")).click();
 
+        driver.findElement(By.xpath("//input[@id='pwd']")).sendKeys(password);
+
+        System.out.print(username + " " +password);
+
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
     }
 
     @Then(": User should be navigate to home page.")
     public void user_should_be_navigate_to_home_page() throws InterruptedException {
-        System.out.println("Then");
-
+        System.out.println(" Then");
         Thread.sleep(5000);
         driver.close();
     }
-
-
 }
--- src/test/resources/features/LoginUser.feature
+++ src/test/resources/features/LoginUser.feature
@@ -1,14 +1,13 @@
-@SmokeScenario
+@SmokeTest
 Feature: User Login Functionality
 
-  @SmokeTest
   Scenario Outline: Validate user is able to login
 
     Given :User navigates to login page.
     When : User enters <username> and <password>
     Then : User should be navigate to home page.
 
-   Examples:
-     |username  |password|
-     |user1     |pass1   |
-     |user2     |pass2   |
+    Examples:
+      | username | password |
+      | user1    | pass1    |
+      | user2    | pass2    |
